8021b6252961543de3f1f3e4ffeb555c679b37c1
andreygalassi <andreygalassi@gmail.com>
2017-06-16 23:35:21 -0300

    comandos de interação

--- JuizJokenpo/src/main/java/br/buscape/JuizJokenpo/App.java
+++ JuizJokenpo/src/main/java/br/buscape/JuizJokenpo/App.java
@@ -2,10 +2,24 @@ package br.buscape.JuizJokenpo;
 
 public class App {
     
-	public static void main( String[] args ){
+	public static void main( String... args ){
     
-		
-		
+		if (args.length!=2){
+			System.out.println("Informe as 2 armas. Elas podem ser PEDRA, PAPEL, TESOURA");
+			return ;
+		}
+
+		try {
+			Arma arma1 = Arma.getArma(args[0]);
+			Arma arma2 = Arma.getArma(args[1]);
+			
+			Juiz juiz = new Juiz();
+			
+			System.out.println(juiz.joKenPo(arma1, arma2));
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 		
     }
 }
